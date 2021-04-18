04bcc59a99b909cfb4e0bef49199fac5db0e787e
Arpit <arpit.rai@viayam.today>
2021-04-19 02:56:59 +0530

    Added express.static('build') to app.use method

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('build'));
 
 morgan.token('body', function (req, res) {
     console.log(req.body);
